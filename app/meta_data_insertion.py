71de5f1d13cda69e4233a9d3816fdd3548a87503
saikat some <saikatsome038@gmail.com>
2026-04-16 18:38:36 +0530

    add RAIS button

--- app/meta_data_insertion.py
+++ app/meta_data_insertion.py
@@ -35,7 +35,7 @@ with col1:
     st.markdown("""
     <div style="
         text-align:right;
-        margin-top:-260px;   /* 🔥 move upward */
+        margin-top:-240px;   /* 🔥 move upward */
         margin-bottom:10px;
     ">
         <a href="https://ris-dev-rvx5qbbut4mydxxnkzn5fz.streamlit.app/" target="_blank"
