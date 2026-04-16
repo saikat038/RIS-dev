47fb307dd32e6e98241b4b6cdb82c8007507097d
saikat some <saikatsome038@gmail.com>
2026-04-16 17:59:15 +0530

    added train model button

--- app/main.py
+++ app/main.py
@@ -93,7 +93,7 @@ st.write("")
 st.markdown("""
 <div style="
     text-align:right;
-    margin-top:-125px;   /* 🔥 move upward */
+    margin-top:-165px;   /* 🔥 move upward */
     margin-bottom:10px;
 ">
     <a href="https://ris-dev-metainsertion.streamlit.app/" target="_blank"
