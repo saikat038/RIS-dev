4fde7681745ef068f392e342862f5bd7563b6880
saikat some <saikatsome038@gmail.com>
2026-04-16 19:07:20 +0530

    fix train model

--- app/main.py
+++ app/main.py
@@ -97,7 +97,7 @@ st.write("")
 st.markdown("""
 <div style="
     text-align:right;
-    margin-top:-240px;   /* 🔥 move upward */
+    margin-top:-230px;   /* 🔥 move upward */
     margin-bottom:10px;
 ">
     <a href="https://ris-dev-metainsertion.streamlit.app/" target="_blank"
